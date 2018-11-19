595ac274139db169a6bac66fa9370d09304fd7fb
Bala Krishna Ragala <baluragala@gmail.com>
2018-11-19 11:46:39 +0530

    rest

---
+++ rest.js
@@ -0,0 +1,15 @@
+function sum() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  console.log(total);
+}
+
+function sum(a, b, ...args) {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  console.log(total);
+}
