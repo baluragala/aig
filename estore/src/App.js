ad1a585e6b8b28d9bd51904b91e79d2ac06b9d68
Bala Krishna Ragala <baluragala@gmail.com>
2018-11-21 12:11:51 +0530

    JSX

--- estore/src/App.js
+++ estore/src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
-import Header from "./Header";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Vismaya welcomes you!!"
+    };
+    this.fruits = ["Apple", "Banana", "Chikoo"];
+  }
+
+  getMessage() {
+    return "This is your message";
+  }
+
   render() {
     return (
-      <div>
-        <Header />
+      <div style={{ backgroundColor: "lightblue", border: "1px solid green" }}>
+        <p>{1 + 2}</p>
+        <p>{"hello".toUpperCase()}</p>
+        <p>{this.getMessage()}</p>
+        <p>{1 > 10 ? "yes" : "no"}</p>
+        <ul>
+          {this.fruits.map(f => (
+            <li className="item">{f}</li>
+          ))}
+        </ul>
       </div>
     );
   }
--- estore/src/Header.js
+++ estore/src/Header.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
+    let mainHead = "E-Store";
     return (
       <header>
-        <h1>E-Store</h1>
+        <h1>{mainHead}</h1>
         <h3>An Exclusive Store For All Electronics</h3>
       </header>
     );
