2c900fee55eca6d3102c52c2042574edc40faaf4
Bala Krishna Ragala <baluragala@gmail.com>
2018-11-22 08:43:50 +0530

    uncontrolled component

--- estore/src/components/AddProduct.js
+++ estore/src/components/AddProduct.js
@@ -7,6 +7,8 @@ class AddProduct extends Component {
       title: "dell xps"
     };
     this.handleChange = this.handleChange.bind(this);
+    this.setRef = this.setRef.bind(this);
+    console.log("ele", document.querySelectorAll("input")[0]);
   }
 
   handleChange(e) {
@@ -14,16 +16,26 @@ class AddProduct extends Component {
     this.setState({ title: e.target.value });
   }
 
+  setRef(ref) {
+    this.priceRef = ref;
+    console.dir(ref);
+  }
+
   render() {
     console.log("render-AddProduct");
     return (
       <div>
+        <button onClick={() => alert(this.priceRef.value)}>GET</button>
+        <button onClick={() => (this.priceRef.value = Math.random())}>
+          SET
+        </button>
         <form>
           <input
             type="text"
             value={this.state.title}
             onChange={this.handleChange}
           />
+          <input type="number" ref={this.setRef} />
         </form>
       </div>
     );
